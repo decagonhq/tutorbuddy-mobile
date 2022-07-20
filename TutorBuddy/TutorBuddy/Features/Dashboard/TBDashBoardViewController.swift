77ab0b455995d49a97b269d985336050a5eebbe1
Ugo Val <ugoval93@gmail.com>
2022-07-20 11:57:19 +0100

    tbs

---
+++ TutorBuddy/TutorBuddy/Features/Dashboard/TBDashBoardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TBDashBoardViewController.swift
+//  TutorBuddy
+//
+//  Created by  Ugo  Val on 20/07/2022.
+//
+
+import UIKit
+
+class TBDashBoardViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
